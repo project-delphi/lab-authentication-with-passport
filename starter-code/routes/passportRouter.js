967fdf51027738f78c2bb2fcfc26247a18bbee1f
Ravi Kalia <ravkalia@gmail.com>
2018-02-19 12:30:54 -0600

    signup feature complete

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,22 +1,57 @@
-const express        = require("express");
-const router         = express.Router();
+const express        = require('express')
+const router         = express.Router()
 // User model
-const User           = require("../models/user");
+const User           = require('../models/user')
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const bcrypt         = require('bcrypt')
+const bcryptSalt     = 10
+const ensureLogin = require('connect-ensure-login')
+const passport      = require('passport')
 
 
 
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+
+
+router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render('passport/private', { user: req.user })
+})
+
+router.get('/signup',(req,res,next) => {
+  res.render('passport/signup')  
+})
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
 });
 
 
-
-
-
-
-module.exports = router;
+module.exports = router
